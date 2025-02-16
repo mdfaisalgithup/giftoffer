c454819efd0ad85beb82a251ace92d0187a084d0
mdfaisalgithupe <mdfaisald0706@gmail.com>
2025-02-16 13:08:34 +0600

    p

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "./header/page";
 import Footer from "./footer/page";
+import Head from "next/head";
 
 
 
@@ -24,10 +25,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">   
 
+<link rel="icon" href="https://i.ibb.co.com/9HSTVWfS/39219854-35e8-4bc4-a275-bef34da6ab88.jpg" sizes="any" />
 
       <body
 
-        className="font-sans font-lato">
+        className="font-lato">
           <Header/>
         {children}
         <Footer/>
